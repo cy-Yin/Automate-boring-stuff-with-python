317a9dd5a8646d1c69e5a94b8ac67135d7986956
yinchaoyang <YCY2857959697@hotmail.com>
2022-07-09 23:41:34 +0800

    chapter13 -- combinePdfs.py

---
+++ chapter13/combinePdfs.py
@@ -0,0 +1,28 @@
+#! python
+# Combines all the PDFs in the current working directory into a single PDF.
+
+import PyPDF2, os
+
+# Get all the PDF filenames.
+pdfFiles = []
+for filename in os.listdir('chapter13/'):
+    if filename.endswith('.pdf'):
+        pdfFiles.append(filename)
+pdfFiles.sort(key=str.lower)
+
+pdfWriter = PyPDF2.PdfFileWriter()
+
+# Loop through all the PDF files.
+for filename in pdfFiles:
+    pdfFileObj = open(filename, 'rb')
+    pdfReader = PyPDF2.PdfFileReader(pdfFileObj)
+
+    # Loop through all the pages (except the first) and add them.
+    for pageNum in range(1, pdfReader.numPages):
+        pageObj = pdfReader.getPage(pageNum)
+        pdfWriter.addPage(pageObj)
+
+# Save the resulting PDF in a file.
+pdfOutput = open(r'chapter13/allminutes.pdf', 'wb')
+pdfWriter.write(pdfOutput)
+pdfOutput.close()
